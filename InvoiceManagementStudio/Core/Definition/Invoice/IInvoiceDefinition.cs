154766a92ab551c238420455a84f50a954600a05
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-10-28 19:20:12 +0100

    add invoice flat attributes

--- InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceDefinition.cs
+++ InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceDefinition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using InvoiceManagementStudio.Core.Definition.Entity;
@@ -9,6 +10,16 @@ namespace InvoiceManagementStudio.Core.Definition.Invoice
     public interface IInvoiceDefinition
     {
         IObjectIdentifier<ulong> Id { get; }
+        string Serial { get; }
+        DateTime IssueDate { get; }
+        DateTime DueDate { get; }
+        DateTime? RedemptionDate { get; }
+        bool IsPayed { get; }
+        decimal Subtotal { get; }
+        decimal Discount { get; }
+        decimal Total { get; }
+        decimal Payed { get; }
+        decimal Due { get; }
         List<IInvoiceItemDefinition> Items { get; }
         List<IInvoicePaymentDefinition> Payments { get; }
         IEntitySupplierDefinition Supplier { get; }

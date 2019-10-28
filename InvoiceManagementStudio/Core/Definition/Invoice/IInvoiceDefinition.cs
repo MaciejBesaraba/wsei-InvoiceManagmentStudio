d27498d794cea358a26b68b5ebc79d8b0fbb0484
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-10-28 19:33:41 +0100

    add summaries

--- InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceDefinition.cs
+++ InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceDefinition.cs
@@ -7,22 +7,84 @@ using InvoiceManagementStudio.Core.Definition.Entity;
 namespace InvoiceManagementStudio.Core.Definition.Invoice
 {
 
+    /// <summary>
+    /// Defines object model for invoice 
+    /// </summary>
     public interface IInvoiceDefinition
     {
+        /// <summary>
+        /// Unique logical identifier of invoice
+        /// </summary>
         IObjectIdentifier<ulong> Id { get; }
+
+        /// <summary>
+        /// Unique fiscal identifier of invoice
+        /// </summary>
         string Serial { get; }
+
+        /// <summary>
+        /// Date when invoice was issued to Receiver
+        /// </summary>
         DateTime IssueDate { get; }
+
+        /// <summary>
+        /// Date when invoice should be payed
+        /// </summary>
         DateTime DueDate { get; }
+
+        /// <summary>
+        /// Date when invoice was fully paid of
+        /// </summary>
         DateTime? RedemptionDate { get; }
+
+        /// <summary>
+        /// Is invoice already payed
+        /// </summary>
         bool IsPayed { get; }
+
+        /// <summary>
+        /// Full payment of services, excluding any discounts
+        /// </summary>
         decimal Subtotal { get; }
+
+        /// <summary>
+        /// Total amount discounted for invoice
+        /// </summary>
         decimal Discount { get; }
+
+        /// <summary>
+        /// Final payment for invoice including discounts
+        /// </summary>
         decimal Total { get; }
+
+        /// <summary>
+        /// Current amount payed by invoice receiver
+        /// </summary>
         decimal Payed { get; }
+
+        /// <summary>
+        /// Amount left to pay by invoice receiver
+        /// </summary>
         decimal Due { get; }
+
+        /// <summary>
+        /// List of invoice services and items shipped to receiver
+        /// </summary>
         List<IInvoiceItemDefinition> Items { get; }
+
+        /// <summary>
+        /// List of payments to supplier issuing invoice
+        /// </summary>
         List<IInvoicePaymentDefinition> Payments { get; }
+
+        /// <summary>
+        /// Entity issuing invoice
+        /// </summary>
         IEntitySupplierDefinition Supplier { get; }
+
+        /// <summary>
+        /// Entity receiving invoice
+        /// </summary>
         IEntityReceiverDefinition Receiver { get; }
     }
 

2f483db5b3609785f4696dcd2692596ade374d9a
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-11-15 20:51:01 +0100

    defined InvoiceItem

--- InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceItemDefinition.cs
+++ InvoiceManagementStudio/Core/Definition/Invoice/IInvoiceItemDefinition.cs
@@ -3,7 +3,19 @@ namespace InvoiceManagementStudio.Core.Definition.Invoice
 
     public interface IInvoiceItemDefinition
     {
-        
+        IObjectIdentifier<ulong> Id { get; }
+
+        string Name { get; }
+
+        decimal UnitPrice { get; }
+
+        EUnitType UnitType { get; }
+
+        decimal Discount { get; }
+
+        decimal Subtotal { get; }
+
+        decimal Total { get; }
     }
 
 }

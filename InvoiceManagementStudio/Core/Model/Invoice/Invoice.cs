7c9523ce0667356daf5057915b526699ddcd1284
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-11-15 10:39:32 +0100

    rename class members to match rules

--- InvoiceManagementStudio/Core/Model/Invoice/Invoice.cs
+++ InvoiceManagementStudio/Core/Model/Invoice/Invoice.cs
@@ -15,31 +15,31 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
     {
         #region class members
 
-        private readonly IObjectIdentifier<ulong> id;
-        private readonly DateTime issueDate;
-        private readonly DateTime dueDate;
-        private readonly DateTime? redemptionDate;
-        private readonly List<IInvoiceItemDefinition> items;
-        private readonly List<IInvoicePaymentDefinition> payments;
-        private readonly IEntitySupplierDefinition supplier;
-        private readonly IEntityReceiverDefinition receiver;
+        private readonly IObjectIdentifier<ulong> _id;
+        private readonly DateTime _issueDate;
+        private readonly DateTime _dueDate;
+        private readonly DateTime? _redemptionDate;
+        private readonly List<IInvoiceItemDefinition> _items;
+        private readonly List<IInvoicePaymentDefinition> _payments;
+        private readonly IEntitySupplierDefinition _supplier;
+        private readonly IEntityReceiverDefinition _receiver;
 
 
-        public IObjectIdentifier<ulong> Id => id;
+        public IObjectIdentifier<ulong> Id => _id;
 
-        public DateTime IssueDate => issueDate;
+        public DateTime IssueDate => _issueDate;
 
-        public DateTime DueDate => dueDate;
+        public DateTime DueDate => _dueDate;
 
-        public DateTime? RedemptionDate => redemptionDate;
+        public DateTime? RedemptionDate => _redemptionDate;
 
-        public List<IInvoiceItemDefinition> Items => items;
+        public List<IInvoiceItemDefinition> Items => _items;
 
-        public List<IInvoicePaymentDefinition> Payments => payments;
+        public List<IInvoicePaymentDefinition> Payments => _payments;
 
-        public IEntitySupplierDefinition Supplier => supplier;
+        public IEntitySupplierDefinition Supplier => _supplier;
 
-        public IEntityReceiverDefinition Receiver => receiver;
+        public IEntityReceiverDefinition Receiver => _receiver;
 
 
         #endregion
@@ -57,14 +57,14 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
             IEntityReceiverDefinition receiver
         )
         {
-            this.id = id;
-            this.issueDate = issueDate;
-            this.dueDate = dueDate;
-            this.redemptionDate = redemptionDate;
-            this.items = items;
-            this.payments = payments;
-            this.supplier = supplier;
-            this.receiver = receiver;
+            this._id = id;
+            this._issueDate = issueDate;
+            this._dueDate = dueDate;
+            this._redemptionDate = redemptionDate;
+            this._items = items;
+            this._payments = payments;
+            this._supplier = supplier;
+            this._receiver = receiver;
         }
 
 
@@ -129,14 +129,14 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
                 return true;
             }
             // TODO ArBy redemptionDate equality
-            return Equals(id, other.id) &&
-                   issueDate.Equals(other.issueDate) &&
-                   dueDate.Equals(other.dueDate) &&
-                   redemptionDate.Equals(other.redemptionDate) &&
-                   Equals(items, other.items) &&
-                   Equals(payments, other.payments) &&
-                   Equals(supplier, other.supplier) &&
-                   Equals(receiver, other.receiver);
+            return Equals(_id, other._id) &&
+                   _issueDate.Equals(other._issueDate) &&
+                   _dueDate.Equals(other._dueDate) &&
+                   _redemptionDate.Equals(other._redemptionDate) &&
+                   Equals(_items, other._items) &&
+                   Equals(_payments, other._payments) &&
+                   Equals(_supplier, other._supplier) &&
+                   Equals(_receiver, other._receiver);
         }
 
         public override bool Equals(object obj)
@@ -157,14 +157,14 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
         {
             unchecked
             {
-                var hashCode = (id != null ? id.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ issueDate.GetHashCode();
-                hashCode = (hashCode * 397) ^ dueDate.GetHashCode();
-                hashCode = (hashCode * 397) ^ redemptionDate.GetHashCode();
-                hashCode = (hashCode * 397) ^ (items != null ? items.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (payments != null ? payments.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (supplier != null ? supplier.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (receiver != null ? receiver.GetHashCode() : 0);
+                var hashCode = (_id != null ? _id.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ _issueDate.GetHashCode();
+                hashCode = (hashCode * 397) ^ _dueDate.GetHashCode();
+                hashCode = (hashCode * 397) ^ _redemptionDate.GetHashCode();
+                hashCode = (hashCode * 397) ^ (_items != null ? _items.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (_payments != null ? _payments.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (_supplier != null ? _supplier.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (_receiver != null ? _receiver.GetHashCode() : 0);
                 return hashCode;
             }
         }

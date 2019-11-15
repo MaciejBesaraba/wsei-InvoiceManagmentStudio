0b9a4377b9e060578d3a0f61162bdba7c86d544f
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-11-15 14:51:49 +0100

    add summaries

--- InvoiceManagementStudio/Core/Definition/Repository/ICrudRepository.cs
+++ InvoiceManagementStudio/Core/Definition/Repository/ICrudRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace InvoiceManagementStudio.Core.Definition.Repository
 {
-
     /// <summary>
     /// Defines generic CRUD service
     /// </summary>
@@ -11,16 +10,45 @@ namespace InvoiceManagementStudio.Core.Definition.Repository
     /// <typeparam name="T">Type of entity for which service is implemented</typeparam>
     public interface ICrudRepository<in TId, T>
     {
+        /// <summary>
+        /// Access all entities stored in memory and returns they as list
+        /// </summary>
+        /// <returns>List of entities</returns>
         List<T> FindAll();
 
+        /// <summary>
+        /// Returns entity stored in memory searched by provided id
+        /// </summary>
+        /// <param name="id">Unique identification of entity</param>
+        /// <returns>Single entity found in memory</returns>
         T FindById(IObjectIdentifier<TId> id);
 
-        T Create(T entity);
+        /// <summary>
+        /// Saves provided entity into memory
+        /// </summary>
+        /// <param name="entity">Valid entity instance</param>
+        /// <returns>Entity that have been successfully stored</returns>
+        T Save(T entity);
 
+        /// <summary>
+        /// Replaced entity stored with provided, searched by entity id
+        /// </summary>
+        /// <param name="entity">Valid entity instance</param>
+        /// <returns>Entity that have been successfully stored</returns>
         T Update(T entity);
 
+        /// <summary>
+        /// Permanently deletes entity stored in memory with id matching provided
+        /// </summary>
+        /// <param name="id">Entity unique identification</param>
+        /// <returns>Entity sored under provided id (for revert possibility)</returns>
         T Delete(IObjectIdentifier<TId> id);
 
+        /// <summary>
+        /// Permanently deletes entity stored in memory and matching provided
+        /// </summary>
+        /// <param name="entity">Valid entity instance</param>
+        /// <returns>Entity provided (for revert possibility)</returns>
         T Delete(T entity);
     }
 

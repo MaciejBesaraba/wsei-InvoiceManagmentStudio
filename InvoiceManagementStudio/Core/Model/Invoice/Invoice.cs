7ac6a57c87f04d056280ee0c1c14841f151216f7
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-11-15 08:32:43 +0100

    add class properties

--- InvoiceManagementStudio/Core/Model/Invoice/Invoice.cs
+++ InvoiceManagementStudio/Core/Model/Invoice/Invoice.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
 using InvoiceManagementStudio.Core.Definition;
 using InvoiceManagementStudio.Core.Definition.Entity;
 using InvoiceManagementStudio.Core.Definition.Invoice;
@@ -10,16 +12,18 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
 
     public class Invoice : IInvoiceDefinition
     {
+        #region class members
+
         private readonly IObjectIdentifier<ulong> id;
         private readonly DateTime issueDate;
         private readonly DateTime dueDate;
         private readonly DateTime? redemptionDate;
-        private readonly bool isPayed;
         private readonly decimal payed;
         private readonly List<IInvoiceItemDefinition> items;
         private readonly List<IInvoicePaymentDefinition> payments;
         private readonly IEntitySupplierDefinition supplier;
         private readonly IEntityReceiverDefinition receiver;
+
 
         public IObjectIdentifier<ulong> Id => id;
 
@@ -30,7 +34,7 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
         public DateTime? RedemptionDate => redemptionDate;
 
         public decimal Payed => payed;
-        
+
         public List<IInvoiceItemDefinition> Items => items;
 
         public List<IInvoicePaymentDefinition> Payments => payments;
@@ -39,12 +43,14 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
 
         public IEntityReceiverDefinition Receiver => receiver;
 
+        #endregion
+        #region constructors
+
         public Invoice(
             IObjectIdentifier<ulong> id,
             DateTime issueDate,
             DateTime dueDate,
             DateTime? redemptionDate,
-            bool isPayed,
             decimal payed,
             List<IInvoiceItemDefinition> items,
             List<IInvoicePaymentDefinition> payments,
@@ -56,13 +62,29 @@ namespace InvoiceManagementStudio.Core.Model.Invoice
             this.issueDate = issueDate;
             this.dueDate = dueDate;
             this.redemptionDate = redemptionDate;
-            this.isPayed = isPayed;
             this.payed = payed;
             this.items = items;
             this.payments = payments;
             this.supplier = supplier;
             this.receiver = receiver;
         }
+
+        #endregion
+        #region properties
+
+        public string Serial => $"FV/{IssueDate.Year.ToString()}/{Id}";
+
+        public decimal Total => Items.Sum(item => item.Total);
+
+        public decimal Subtotal => Items.Sum(item => item.Subtotal);
+
+        public decimal Discount => Items.Sum(item => item.Discount);
+
+        public decimal Due => Total - Payed;
+
+        public bool IsPayed => Total >= Payed;
+
+        #endregion
 
     }
 

84b36ddd439c0801afcb8ad5813ae0a167dfa07b
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-11-16 20:40:38 +0100

    implement invoicePaymentDefinition

--- InvoiceManagementStudio/Core/Definition/Payment/IInvoicePaymentDefinition.cs
+++ InvoiceManagementStudio/Core/Definition/Payment/IInvoicePaymentDefinition.cs
@@ -1,9 +1,44 @@
+using System;
+using System.Collections.Generic;
+
 namespace InvoiceManagementStudio.Core.Definition.Payment
 {
-
+    /// <summary>
+    /// Defines object details for the payments
+    /// </summary>
     public interface IInvoicePaymentDefinition
     {
-        
+        /// <summary>
+        /// Unique logical identifier of invoice (connects to InvoicePaymentDefinition)
+        /// </summary>
+        IObjectIdentifier<ulong> Id { get; }
+
+        /// <summary>
+        /// Specifies payment methods
+        /// </summary>
+        EPaymentType PaymentType { get; }
+
+        /// <summary>
+        /// The date payment was posted
+        /// </summary>
+        DateTime PaymentDate { get; }
+
+        /// <summary>
+        /// Specifies Time zone
+        /// </summary>
+        string LocalTime { get; }
+
+        /// <summary>
+        /// Amount of the money posted (need to be compared with Total payment)
+        /// </summary>
+        decimal PaymentAmount { get; }
+
+        /// <summary>
+        /// Brand of the credit card used to issue payment
+        /// </summary>
+        string CreditCardType { get; }
+
+
     }
 
 }

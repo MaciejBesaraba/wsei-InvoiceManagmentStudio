8510845ba1cb144439b310310326d5686d77957d
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-11-16 13:28:19 +0100

    delete unused import

--- InvoiceManagementStudio/Model/Invoice/Invoice.cs
+++ InvoiceManagementStudio/Model/Invoice/Invoice.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using InvoiceManagementStudio.Core.Definition;
-using InvoiceManagementStudio.Core.Definition.Entity;
 using InvoiceManagementStudio.Core.Definition.Entity.Receiver;
 using InvoiceManagementStudio.Core.Definition.Entity.Supplier;
 using InvoiceManagementStudio.Core.Definition.Invoice;
 using InvoiceManagementStudio.Core.Definition.Item;
 using InvoiceManagementStudio.Core.Definition.Payment;
+
 
 namespace InvoiceManagementStudio.Model.Invoice
 {

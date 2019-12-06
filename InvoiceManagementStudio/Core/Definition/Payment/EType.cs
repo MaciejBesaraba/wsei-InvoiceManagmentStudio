c4f6f2fcc5a7e187f78cfda30a0a6276dda0aad2
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-12-06 17:57:43 +0100

    add changes IInvoicePayment

---
+++ InvoiceManagementStudio/Core/Definition/Payment/EType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementStudio.Core.Definition.Payment
+{
+
+    /// <summary>
+    /// Type of payment used on invoice
+    /// </summary>
+    public enum EType
+    {
+        Cash,
+        CreditCard,
+        BankTransfer,
+        InAdvance
+
+    }
+
+}
--- InvoiceManagementStudio/Core/Definition/Payment/IInvoicePaymentDefinition.cs
+++ InvoiceManagementStudio/Core/Definition/Payment/IInvoicePaymentDefinition.cs
@@ -16,7 +16,7 @@ namespace InvoiceManagementStudio.Core.Definition.Payment
         /// <summary>
         /// Specifies payment methods
         /// </summary>
-        EPaymentType PaymentType { get; }
+        EType PaymentType { get; }
 
         /// <summary>
         /// The date payment was posted

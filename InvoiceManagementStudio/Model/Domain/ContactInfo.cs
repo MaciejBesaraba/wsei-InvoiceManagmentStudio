9889d1c9ca85b8fbe4404f6ee2c74a3ce3f97330
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-12-18 19:42:30 +0100

    add properties and constructor

--- InvoiceManagementStudio/Model/Domain/ContactInfo.cs
+++ InvoiceManagementStudio/Model/Domain/ContactInfo.cs
@@ -1,6 +1,8 @@
 using System;
-
+using InvoiceManagementStudio.Core.Definition;
 using InvoiceManagementStudio.Core.Definition.ContactInfo;
+using InvoiceManagementStudio.Core.Definition.Entity;
+
 
 
 namespace InvoiceManagementStudio.Model.Domain
@@ -8,7 +10,38 @@ namespace InvoiceManagementStudio.Model.Domain
 
     public class ContactInfo : IContactInfoDefinition, IEquatable<ContactInfo>
     {
-        // TODO MaBa
-    }
+        public IObjectIdentifier<ulong> Id { get; }
+        public string Email { get; }
+        public string Phone { get; }
+        public string Mobile { get; }
+        public string Title { get; }
+        public ESex Sex { get; }
+        public string Name { get; }
+        public string Surname { get; }
 
-}
+
+        public ContactInfo(
+            IObjectIdentifier<ulong> id,
+            string email,
+            string phone,
+            string mobile,
+            string title,
+            ESex sex,
+            string name,
+            string surname
+
+        )
+        {
+            Id = id;
+            Email = email;
+            Phone = phone;
+            Mobile = mobile;
+            Title = title;
+            Sex = sex;
+            Name = name;
+            Surname = surname;
+
+
+        }
+
+

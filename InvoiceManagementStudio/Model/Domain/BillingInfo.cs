3f6f6547a7c0c36dc732b963a4b02d9fe0be06c8
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-12-18 19:08:39 +0100

    add properties and constructor

--- InvoiceManagementStudio/Model/Domain/BillingInfo.cs
+++ InvoiceManagementStudio/Model/Domain/BillingInfo.cs
@@ -1,6 +1,15 @@
 using System;
-
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using InvoiceManagementStudio.Core.Definition;
+using InvoiceManagementStudio.Core.Definition.Entity.Receiver;
+using InvoiceManagementStudio.Core.Definition.Entity.Supplier;
+using InvoiceManagementStudio.Core.Definition.Invoice;
+using InvoiceManagementStudio.Core.Definition.Item;
+using InvoiceManagementStudio.Core.Definition.Payment;
 using InvoiceManagementStudio.Core.Definition.BillingInfo;
+using InvoiceManagementStudio.Core.Definition.Address;
 
 
 namespace InvoiceManagementStudio.Model.Domain
@@ -8,7 +17,26 @@ namespace InvoiceManagementStudio.Model.Domain
 
     public class BillingInfo : IBillingInfoDefinition, IEquatable<BillingInfo>
     {
-        // TODO MaBa
-    }
+        public IObjectIdentifier<ulong> Id { get; }
+        public string CompanyName { get; }
+        public string ZipCode { get; }
+        public IAddressDefinition BillingAddress { get; }
 
-}
+
+        public BillingInfo(
+            IObjectIdentifier<ulong> id,
+            string companyName,
+            string zipCode,
+            IAddressDefinition billingAddress
+
+        )
+        {
+            Id = id;
+            CompanyName = companyName;
+            ZipCode = zipCode;
+            BillingAddress = billingAddress;
+
+
+        }
+
+

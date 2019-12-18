3c6626ed4e703c9af03bb385e48ead3dd81641b4
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-12-18 18:24:26 +0100

    add properties and constructor

--- InvoiceManagementStudio/Model/Domain/Address.cs
+++ InvoiceManagementStudio/Model/Domain/Address.cs
@@ -1,6 +1,13 @@
 using System;
-
-using InvoiceManagementStudio.Core.Definition.Address;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using InvoiceManagementStudio.Core.Definition;
+using InvoiceManagementStudio.Core.Definition.Entity.Receiver;
+using InvoiceManagementStudio.Core.Definition.Entity.Supplier;
+using InvoiceManagementStudio.Core.Definition.Invoice;
+using InvoiceManagementStudio.Core.Definition.Item;
+using InvoiceManagementStudio.Core.Definition.Payment;
 
 
 namespace InvoiceManagementStudio.Model.Domain
@@ -8,7 +15,29 @@ namespace InvoiceManagementStudio.Model.Domain
 
     public class Address : IAddressDefinition, IEquatable<Address>
     {
-        // TODO MaBa
-    }
+        public IObjectIdentifier<ulong> Id { get; }
+        public string Country { get; }
+        public string City { get; }
+        public string Street { get; }
+        public string State { get; }
+        public string BuildingNumber { get; }
+        public string FlatNumber { get; }
 
-}
+        public Address(
+            IObjectIdentifier<ulong> id,
+            string name,
+            decimal unitPrice,
+            EUnitType unitType,
+            decimal discount,
+            double quantity
+        )
+        {
+            Id = id;
+            Country = country;
+            City = city;
+            Street = street;
+            State = state;
+            BuildingNumber = buildingNumber;
+            FlatNumber = flatNumber;
+
+        }

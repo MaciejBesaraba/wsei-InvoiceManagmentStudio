c12c9ba96ffe6e1fb3a7240b00f77c6668b2d56e
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-12-25 07:51:38 +0100

    add constructors to receiver and supplier

--- InvoiceManagementStudio/Model/Domain/EntityReceiver.cs
+++ InvoiceManagementStudio/Model/Domain/EntityReceiver.cs
@@ -1,9 +1,23 @@
+using InvoiceManagementStudio.Core.Definition;
+using InvoiceManagementStudio.Core.Definition.BillingInfo;
+using InvoiceManagementStudio.Core.Definition.ContactInfo;
+
+
 namespace InvoiceManagementStudio.Model.Domain
 {
 
     public class EntityReceiver : Entity
     {
-        // ..
+
+        public EntityReceiver(
+            IObjectIdentifier<ulong> id,
+            IBillingInfoDefinition billingInfo,
+            IContactInfoDefinition contactInfo
+        ) : base(id, billingInfo, contactInfo)
+        {
+            
+        }
+
     }
 
 }

3d7dab6eba156f097229384078a7ce6c260bf6f4
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2020-01-04 17:29:55 +0100

    get only

--- Core/Domain/ContactInfo/ContactInfoDto.cs
+++ Core/Domain/ContactInfo/ContactInfoDto.cs
@@ -14,6 +14,37 @@ namespace Core.Domain.ContactInfo
         private readonly string _name;
         private readonly string _surname;
 
+        public IObjectIdentifier<ulong> Id => _id;
+        public string Email => _email;
+        public string Phone => _phone;
+        public string Mobile => _mobile;
+        public string Title => _title;
+        public ESex Sex => _sex;
+        public string Name => _name;
+        public string Surname => _surname;
+
+
+        private ContactInfoDto(
+            IObjectIdentifier<ulong> id,
+            string email,
+            string phone,
+            string mobile,
+            string title,
+            ESex sex,
+            string name,
+            string surname
+        )
+        {
+            _id = id;
+            _email = email;
+            _phone = phone;
+            _mobile = mobile;
+            _title = title;
+            _sex = sex;
+            _name = name;
+            _surname = surname;
+        }
+
     }    
 
 }

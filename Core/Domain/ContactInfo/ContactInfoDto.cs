9117894cbe1404105e7731a0a4106ba2f063ab66
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2020-01-04 17:29:13 +0100

    add backing fields

--- Core/Domain/ContactInfo/ContactInfoDto.cs
+++ Core/Domain/ContactInfo/ContactInfoDto.cs
@@ -1,11 +1,19 @@
 using System;
+using Core.Domain.Entity;
 
 namespace Core.Domain.ContactInfo
 {
-
     public class ContactInfoDto : IContactInfoDefinition, IEquatable<ContactInfoDto>
     {
-        
-    }
+        private readonly IObjectIdentifier<ulong> _id;
+        private readonly string _email;
+        private readonly string _phone;
+        private readonly string _mobile;
+        private readonly string _title;
+        private readonly ESex _sex;
+        private readonly string _name;
+        private readonly string _surname;
+
+    }    
 
 }

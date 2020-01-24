fbe0a16b00536d0dfe244b6f197811b95fcc756d
arekbystrowicz <arek.bystrowicz@gamil.com>
2020-01-24 12:08:47 +0100

    change generic type to domain object

--- Core/Application/ContactInfo/IContactInfoReporitory.cs
+++ Core/Application/ContactInfo/IContactInfoReporitory.cs
@@ -1,9 +1,7 @@
-using Core.Domain.ContactInfo;
-
 namespace Core.Application.ContactInfo
 {
 
-    public interface IContactInfoRepository: ICrudRepository<ulong, IContactInfoDefinition>
+    public interface IContactInfoRepository: ICrudRepository<ulong, Domain.ContactInfo.ContactInfo>
     {
         
     }
--- Repository/AbstractRowMapper.cs
+++ Repository/AbstractRowMapper.cs
@@ -3,6 +3,15 @@ using Npgsql;
 
 namespace Repository
 {
+    public interface IRowMapper<T>
+    {
+        List<T> FromResultSet(NpgsqlDataReader resultSet);
+
+        NpgsqlCommand ToRow(NpgsqlCommand command, T entity);
+
+        T FromRow(NpgsqlDataReader resultSet);
+    }
+    
     public abstract class AbstractRowMapper<T>
     {
         protected AbstractRowMapper() { }

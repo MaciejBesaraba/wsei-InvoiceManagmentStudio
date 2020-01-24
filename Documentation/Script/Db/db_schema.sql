a9e8cdd4316c18fdc9b141eafbc4007e7cdff0ee
arekbystrowicz <arek.bystrowicz@gamil.com>
2020-01-24 12:10:32 +0100

    add database create sql for contact info

--- Documentation/Script/Db/db_schema.sql
+++ Documentation/Script/Db/db_schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE ivms;
 
 
 DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS contact_info;
 
 
 CREATE TABLE address (
@@ -12,4 +13,15 @@ CREATE TABLE address (
   state VARCHAR(255) NOT NULL,
   building_number INTEGER NOT NULL,
   flat_number INTEGER
+);
+
+CREATE TABLE contact_info (
+    id SERIAL PRIMARY KEY ,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255),
+    mobile VARCHAR(255),
+    title VARCHAR(255),
+    gender VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255)
 );

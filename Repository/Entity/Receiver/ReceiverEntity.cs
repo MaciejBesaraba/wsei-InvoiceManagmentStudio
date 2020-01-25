3eaa1030979f077edfaa44b155fc8af2dd4446c3
arekbystrowicz <arek.bystrowicz@gamil.com>
2020-01-25 15:02:14 +0100

    implemet receiver eneity

---
+++ Repository/Entity/Receiver/ReceiverEntity.cs
@@ -0,0 +1,31 @@
+using System.Data;
+using Core.Domain;
+using BillingInfoDomain = Core.Domain.BillingInfo.BillingInfo;
+using ContactInfoDomain = Core.Domain.ContactInfo.ContactInfo;
+using ReceiverDomain = Core.Domain.Entity.Receiver.EntityReceiver;
+
+namespace Repository.Entity.Receiver
+{
+    public class ReceiverEntity : EntityEntity
+    {
+        public ReceiverEntity(ulong? id, ulong billingInfo, ulong contactInfo) : base(id, billingInfo, contactInfo) { }
+
+        public ReceiverDomain ToDomain(BillingInfoDomain billingInfo, ContactInfoDomain contactInfo)
+        {
+            return new ReceiverDomain(
+                new SimpleObjectIdentifier(Id ?? throw new DataException("ReceiverEntity Id is null")),
+                billingInfo,
+                contactInfo
+            );
+        }
+
+        public static ReceiverEntity FromDomain(ReceiverDomain domain)
+        {
+            return new ReceiverEntity(
+                domain.Id.Value,
+                domain.BillingInfo.Id.Value,
+                domain.ContactInfo.Id.Value
+            );
+        }
+    }
+}

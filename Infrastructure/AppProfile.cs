c58a77ab436110f1888fd0f908b46c0612c1619a
Maciej Besaraba <maciej.besaraba@microsoft.wsei.edu.pl>
2020-01-30 19:10:27 +0100

    fix

--- Infrastructure/AppProfile.cs
+++ Infrastructure/AppProfile.cs
@@ -1,17 +1,26 @@
+using System.Configuration;
+
 namespace Infrastructure
 {
     public class AppProfile : IAppProfile
     {
         // TODO from properties
-        private const string CDataSource = @"127.0.0.1";
-        private const string CUsername = "postgres";
-        private const string CPassword = "postgres";
-        private const string CDatabase = "ivms";
+        private const string DefaultDataSource = @"127.0.0.1";
+        private const string DefaultUsername = "postgres";
+        private const string DefaultPassword = "postgres";
+        private const string DefaultDatabase = "ivms";
 
-
-        public string DataSource => CDataSource;
-        public string Username => CUsername;
-        public string Password => CPassword;
-        public string Database => CDatabase;
+        public string DataSource { get; }
+        public string Username { get; }
+        public string Password { get; }
+        public string Database { get; }
+        
+        public AppProfile()
+        {
+            DataSource = DefaultDataSource;
+            Username = DefaultUsername;
+            Password = DefaultPassword;
+            Database = DefaultDatabase;
+        }
     }
 }
